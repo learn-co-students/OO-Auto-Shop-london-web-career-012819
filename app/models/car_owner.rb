49442342b1c097b260a7b56a586e1671cdb23c2c
stubrew24 <sewelly@gmail.com>
2019-02-02 23:12:18 +0000

    Done.

--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -2,9 +2,30 @@ class CarOwner
 
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def create_car(make, model, classification, mechanic)
+    Car.new(make, model, classification, self, mechanic)
+  end
 
+  def cars
+    Car.all.select {|x| x.owner == self}
+  end
+
+  def mechanics
+    cars.map {|x| x.mechanic}
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.average
+    Car.all.count.to_f / all.count.to_f
+  end
 end
