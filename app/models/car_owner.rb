4fa984d61cf87edca26d3a5f944b245a1ea10b71
SofiaFredriksson <sofiapkf@gmail.com>
2019-02-03 14:32:11 +0000

    First Try

--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -1,9 +1,30 @@
 class CarOwner
 
-  attr_reader :name
+  attr_reader :name, :car
+
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def cars
+    Car.all.select { |car| car.car_owner == self }
+  end
+
+  def mechanics
+    cars.map { |car| car.mechanic }
+  end
+
+  def self.average_cars
+    arr = @@all.map { |all| all.name  }
+    arr2 = Car.all.map { |all| all.make  }
+    arr2.length.to_f/arr.length.to_f
   end
 
 
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,32 @@ def reload
   load 'config/environment.rb'
 end
 
+m1 = Mechanic.new("Bob", "Antique")
+m2 = Mechanic.new("Karl", "Antique")
+m3 = Mechanic.new("Erika", "Exotic")
+m4 = Mechanic.new("John", "Clunker")
+m5 = Mechanic.new("Charles", "Clunker")
+
+co1 = CarOwner.new("Sofia")
+co2 = CarOwner.new("Erika")
+co3 = CarOwner.new("Ricky")
+co4 = CarOwner.new("Luc")
+co5 = CarOwner.new("Peder")
+co6 = CarOwner.new("Kalle")
+
+# (make, model, classification, car_owner, mechanic)
+
+car1 = Car.new("BMW", "Speed", "Clunker", co1, m4)
+car2 = Car.new("Audi", "A6", "Antique", co2, m2)
+car3 = Car.new("Ferrari", "Fast", "Antique", co3, m1)
+car4 = Car.new("Golf", "Slow", "Clunker", co4, m5)
+car5 = Car.new("Jaguar", "XKR", "Exotic", co5, m3)
+car6 = Car.new("BMW", "M6", "Exotic", co6, m3)
+car7 = Car.new("Toyota", "Prius", "Clunker", co4, m4)
+
+
+
+
+
+
 binding.pry
