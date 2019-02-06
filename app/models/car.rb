bb010d607f9523caaca5c28658152f9491866eaa
Tominiyi Ajayi <tominiyimunito@Tominiyis-MacBook-Air.local>
2019-02-06 13:27:47 +0000

    Done

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,27 @@
 class Car
 
-  attr_reader :make, :model
+  attr_reader :make, :model, :classification, :mechanic, :car_owner
 
-  def initialize(make, model, classification)
+  @@all = []
+
+  def initialize(make, model, classification,mechanic,car_owner)
     @make = make
     @model = model
     @classification = classification
+    @mechanic = mechanic
+    @car_owner = car_owner
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def self.classification
+    @@all.map {|car| car.classification}
+  end
+
+  def mechanics
+    Mechanic.all.select {|expertise| expertise.specialty ==@classification}
+  end
 end
